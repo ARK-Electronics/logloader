29212e5d084d552510e2b5507152c96d68f81d5d
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-03-08 09:47:48 -0900

    MAVSDK emit heartbeats

--- src/LogLoader.cpp
+++ src/LogLoader.cpp
@@ -39,7 +39,8 @@ void LogLoader::stop()
 
 bool LogLoader::wait_for_mavsdk_connection(double timeout_ms)
 {
-	_mavsdk = std::make_shared<mavsdk::Mavsdk>(mavsdk::Mavsdk::Configuration(mavsdk::Mavsdk::ComponentType::CompanionComputer));
+	_mavsdk = std::make_shared<mavsdk::Mavsdk>(mavsdk::Mavsdk::Configuration(1, MAV_COMP_ID_ONBOARD_COMPUTER,
+			true)); // Emit heartbeats (Client)
 	auto result = _mavsdk->add_any_connection(_settings.mavsdk_connection_url);
 
 	if (result != mavsdk::ConnectionResult::Success) {
