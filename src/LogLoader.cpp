95c545cf7b3e9635b8f39abd9d606a0368a26ff8
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-08-24 11:45:41 -0800

    double doh

--- src/LogLoader.cpp
+++ src/LogLoader.cpp
@@ -442,11 +442,11 @@ bool LogLoader::send_log_to_server(const std::string& file_path)
 
 	if (info.second == Protocol::Https) {
 		httplib::SSLClient cli(info.first);
-		res = cli.Post("/upload");
+		res = cli.Post("/upload", items);
 
 	} else {
 		httplib::Client cli(info.first);
-		res = cli.Post("/upload");
+		res = cli.Post("/upload", items);
 	}
 
 	if (res && res->status == 302) {
